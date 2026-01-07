eb898a46c31c82fe3cd9efca34bb31331c893d74
Darshan Prajapati <d23prajapati@gmail.com>
2026-01-07 22:15:03 +0100

    Added Acceleo Model To Text transformation project

---
+++ model/TrafficLightSystem/src_gen/TrafficLightSystem/TrafficLightSystem.ino
@@ -0,0 +1,15 @@
+			const int PIN_D8 = 8;
+			const int PIN_D9 = 9;
+			const int PIN_D10 = 10;
+
+void setup() 
+{
+			pinMode(PIN_D8, OUTPUT);
+			pinMode(PIN_D9, OUTPUT);
+			pinMode(PIN_D10, OUTPUT);
+
+}
+void loop()
+{
+Serial.println("Hello World");
+}
